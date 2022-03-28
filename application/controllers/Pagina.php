cb0e6da6c0a286473068273d76d2e80cddfda650
Renan dos Santos Lima <renansantoslima1998@outlook.com>
2022-03-28 11:36:54 -0300

    Configurações básicas
    Foi feito as configurações básicas das paginas.

---
+++ application/controllers/Pagina.php
@@ -0,0 +1,24 @@
+<?php
+//nome para salvalr um controller é sempre a primeira letra maiuscula
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+//o nome dos arquivos e classes devem ser os mesmos
+class Pagina extends CI_Controller {
+
+	//construtor constroi tudo o que precisa nos projetos
+	function __construct(){
+		parent::__construct();
+		$this->load->helper('url');//carrega em todo o controller 
+	}
+
+	public function index(){
+		//o helper pode ser feito aqui também
+		//$this->load->helper('url');//carrega so em index
+
+
+		//titulo dinamico
+		$dados['titulo'] = 'Manyminds';
+		//chama a view principal
+		$this->load->view('home',$dados);
+	}
+}
---
+++ application/views/topo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo $titulo; ?></title>
+		<meta content-type='text/html' charset="utf-8">
+
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css');?>">
+	</head>
+	<body>
+
+	
