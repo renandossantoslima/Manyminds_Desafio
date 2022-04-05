0f013bb2656c273e3943d8e529a8417b7ba5b484
Renan dos Santos Lima <renansantoslima1998@outlook.com>
2022-04-05 14:54:02 -0300

    Projeto final
    Versão final do projeto.

--- application/views/login.php
+++ application/views/login.php
@@ -33,7 +33,7 @@
 			echo form_submit('enviar','Entrar',array('class' => 'botao'));
 			echo form_close();//fim form
 		?>
-		<span>Obs: Se você não tiver um usuario cadastrado tem pedir que para o administrador que faça o seu cadastro.</span>
+		<span>Obs: Se você não tiver um usuario cadastrado tem que pedir para o administrador que faça o seu cadastro.</span>
 	</div>
 
 <?php
